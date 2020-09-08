b1ab69c7e8002c588012bb1fe8ded45b9a65c038
ashwin <alexwin75@gmail.com>
2020-09-08 12:31:45 -0400

    11 :rocket:

---
+++ jsalgos-fp-11.js
@@ -0,0 +1,10 @@
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+
+  console.log(anim.slice(beginSlice, endSlice));
+  return anim.slice(beginSlice, endSlice);
+
+  // Only change code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
