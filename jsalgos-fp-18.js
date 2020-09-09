9e8b1d4659b51654058d5ef4259042983a7032ad
ashwin <alexwin75@gmail.com>
2020-09-09 12:18:55 -0400

    18 :baby:

---
+++ jsalgos-fp-18.js
@@ -0,0 +1,11 @@
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  let newArray = [];
+  return newArray.concat(arr).sort(function (a, b) {
+    return a === b ? 0 : a < b ? -1 : 1;
+  });
+
+  // Only change code above this line
+}
+console.log(nonMutatingSort(globalArray));
